f0c311561a52eaaa49c15b5efebabb19cf2a1f11
Akshath Jain <akshath.r.jain@gmail.com>
2018-10-28 19:36:54 -0400

    added readme.md

---
+++ Chrome-Extension-WDW/ImageManipulator/popup.js
@@ -0,0 +1,25 @@
+/*
+Name: Akshath Jain
+Date: 10/26/18
+Purpose: js for wdw chrome extension
+*/
+
+//main function -- where everything happens
+function main(){
+	setupInsertButton();
+}
+
+//function that adds click behavior to the insert button
+function setupInsertButton(){
+	//get our button and set on click listener
+	document.getElementById("insert-button").addEventListener("click", function(){
+		chrome.tabs.executeScript({
+			file: "contentScripts.js"
+		});
+	});
+}
+
+//once all the stuff is loaded, call the main function
+document.addEventListener("DOMContentLoaded", function(){
+	main();
+});
