e996f5510cf9ebf41f58911aed3f8d3c4e92ed92
Akshath Jain <akshath.r.jain@gmail.com>
2018-11-04 13:53:59 -0500

    removed extraneous files

--- ImageManipulatorPresentation/contentScripts.js
+++
@@ -1,4 +0,0 @@
-var imageUrl = "https://www.cmu.edu/marcom/brand-standards/images/logos-colors-type/CMULogo_do-min.png";
-var images = document.getElementsByTagName('img');
-for(var i = 0; i < images.length; i++)
-	images[i].src = imageUrl;
